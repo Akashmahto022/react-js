e08f85c76d34b79b0417f511ba41ab6bbc8c923f
Akashmahto022 <akashmahto1169@gmail.com>
2024-06-22 19:47:16 +0530

    learn component in react app and the start building own react library

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import First from './First'
+
+const App = () => {
+  return (
+    <div>
+      <First/>
+    </div>
+    
+  )
+}
+
+
+export default App
---
+++ basic-react-app/src/First.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const First = () => {
+  return (
+    <h2>First react component</h2>
+  )
+}
+
+export default First
